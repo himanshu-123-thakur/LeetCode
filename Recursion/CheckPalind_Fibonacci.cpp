0e81a47c9a27ba4d444c472851b7c76b39e6925a
himanshu-sde-beep <himanshu.thakur@resonous.com>
2026-01-01 20:46:33 +0530

    Fibonacci Number

--- Recursion/CheckPalind_Fibonacci.cpp
+++ Recursion/CheckPalind_Fibonacci.cpp
@@ -1,4 +1,4 @@
-//CODE 
+//CODE - FIBONACCI ??
 #include <iostream>
 using namespace std;
 // void checkPalindrome(int i,string str, int n){
@@ -19,12 +19,25 @@ bool isPalindrome(int i, const string &str) {
     if (str[i] != str[n - i - 1]) return false;
     return isPalindrome(i + 1, str);
 }
+
+int fibbonaci(int n){
+    if(n==0 || n==1){
+        return n;
+    }
+    else{
+        return fibbonaci(n-1)+fibbonaci(n-2);
+    }
+}
 int main(){
-    string str;
-    cin>>str;
-    int i = 0;
-    isPalindrome(i,str);
-    cout<<boolalpha;
-    cout<<str<<" is Palindrome : "<<isPalindrome(i,str)<<endl;
+    // string str;
+    // cin>>str;
+    // int i = 0;
+    // isPalindrome(i,str);
+    // cout<<boolalpha;
+    // cout<<str<<" is Palindrome : "<<isPalindrome(i,str)<<endl;
+    int n;
+    cout<<"Enter size = ";
+    cin>>n;
+    cout<<"The "<<n+1<<" number in series is "<<fibbonaci(n)<<endl;
     return 0; 
 }
