4eb12e8bedc3b74e61ffc6ef457c59f583a6c13e
Himanshu Thakur <himanthakur123@gmail.com>
2026-01-02 17:22:32 +0530

    high and low Frequencies

---
+++ HASHING/HighandLowFreq.cpp
@@ -0,0 +1,37 @@
+//CODE 
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+void high_freq(vector<int>&v , int n){
+    unordered_map<int,int>mpp;
+    int result=0;
+    int ans;
+    for(int i = 0;i<v.size();i++){
+        mpp[v[i]]++;
+        if(mpp[v[i]]>result){
+            result = mpp[v[i]];
+            ans = v[i];
+        }
+        else{
+            continue;
+        }
+
+    }
+
+    cout << "Highest occurring element is " << ans
+     << " repeated " << result << " times\n";
+
+}
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    high_freq(v,v.size());
+    return 0;
+}
