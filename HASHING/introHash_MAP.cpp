453db3eb448aff9ce871936bc1d5ebaf81025a21
Himanshu Thakur <himanthakur123@gmail.com>
2026-01-02 15:53:30 +0530

    Add Hashing Section

---
+++ HASHING/introHash_MAP.cpp
@@ -0,0 +1,32 @@
+//CODE 
+//map<int,int> name_name ;   (key/Number , value/frequency)
+//unordered_map <> name
+//Array hashing is only safe when the value range is known and bounded. Otherwise, use map or unordered_map.
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+
+int main(){
+    int arr[10]={2,23,2,2,3,3,4,2,5,53};
+    int hash[53]={0};
+    string s;
+    cin>>s;
+    map <int, int> hash_map{};
+    for(int i=0;i<s.size();i++){
+        hash_map[s[i]]++;
+    }
+    cout<<"From the string "<<s<<" want to check how many times the folowing things gets repeated : ";
+    char c;
+    cin>>c;
+    cout <<c<<" gets repeated "<<hash_map[c]<<endl;
+
+    for(int i =0;i<10;i++){
+        hash[arr[i]]++;
+    }
+
+    cout <<"The number 2 is repeated "<<hash[2]<<" times "<<endl;
+    cout <<"The number 53 is repeated "<<hash[53]<<endl;
+    return 0;
+}
