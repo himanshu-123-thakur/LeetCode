5d5248cb2abc910d619bdac0f153c0f13c37a7cd
Himanshu Thakur <himanthakur123@gmail.com>
2026-01-04 23:19:08 +0530

    selectionSORT

---
+++ ARRAYS/SORTING/selectionSort.cpp
@@ -0,0 +1,13 @@
+//CODE 
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+// int main(){
+//     vector <int> v {21,3124,43,245,25,245,42,5,345,34,55};
+//     sort(v.begin(),v.end() ,greater<int>());
+//     for(auto &i:v){
+//         cout<<i<<" ";
+//     }
+//     return 0;
+// }
