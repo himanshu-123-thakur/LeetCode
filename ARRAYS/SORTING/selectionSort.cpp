302f09910d1b9027fc7774b19b0c882a38a757aa
Himanshu Thakur <himanthakur123@gmail.com>
2026-01-05 00:15:43 +0530

    selectionSORT

--- ARRAYS/SORTING/selectionSort.cpp
+++ ARRAYS/SORTING/selectionSort.cpp
@@ -11,3 +11,36 @@ using namespace std;
 //     }
 //     return 0;
 // }
+
+class selectionSort{
+    private:
+    public:
+    void sort(int arr[], int size){
+        int min=arr[0];
+        int temp;
+        for(int i =0;i<size;i++){
+            int min = i;
+            for(int j = i+1;j<size;j++){
+                if(arr[j]<arr[min])
+                min = j;
+        }
+        swap(arr[i],arr[min]);
+    }
+        for(int i=0;i<size;i++){
+            cout<<arr[i]<<" ";
+        }
+    }
+};
+int main(){
+    int size ;
+    cin>>size;
+    int arr[size];
+    cout<<"Enter Array of size "<<size<<" : ";
+    for(int i =0; i <size;i++){
+        cin>>arr[i];
+    }
+
+    selectionSort s;
+    s.sort(arr,size);
+    return 0;
+}
