7246da6e0454bc3ab7c094995b0d69329da9be1b
Himanshu Thakur <himanthakur123@gmail.com>
2026-01-05 01:05:26 +0530

    Bubble SORT (ADJACENT SORTING)

--- ARRAYS/SORTING/BubbleSort.cpp
+++ ARRAYS/SORTING/BubbleSort.cpp
@@ -1,19 +1,39 @@
+//CODE - ADJACENT SWAPS
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main(){
+    cout << "RUNNING: BUBBLE-sort-v3\n";
+
     int arr[7]={12,34,653,1,2,45,80};
 
-    for(int i =6;i>=0;i++){
-        int max = i;
-        for(int j=i-1;j>=0;j--){
-            if(arr[j]>arr[max])
-            max = j;
+    // for(int i=6;i>0;i--){
+    //     int maxI = i;
+    //     for(int j=i-1;j>=0;j--){
+    //         if(arr[j]>arr[maxI])
+    //         maxI = j;
+    //     }
+    //     swap(arr[maxI],arr[i]);
+    // }
+
+    // for(int i =0;i<7;i++){
+    //     cout<<arr[i]<<" "<<endl;
+    // }
+    // cout<<endl;
+
+
+    for(int i =sizeof(arr)/sizeof(arr[0])-1;i>=0;i--){{
+        for(int j=0;j<=i-1;j++){
+            if(arr[j]>arr[j+1])
+            swap(arr[j],arr[j+1]);
         }
-        swap(arr[max],arr[i]);
+    }
+
     }
 
     for(int i =0;i<7;i++){
-        cout<<arr[i]<<" ";
+         cout<<arr[i]<<" ";
     }
+    cout<<endl;
     return 0;
 }
