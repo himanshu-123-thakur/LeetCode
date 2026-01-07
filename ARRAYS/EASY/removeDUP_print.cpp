db88d617a13a8dc5685759de282174ec22540476
Himanshu Thakur <himanthakur123@gmail.com>
2026-01-07 14:46:07 +0530

    Check if sorted

---
+++ ARRAYS/EASY/removeDUP_print.cpp
@@ -0,0 +1,20 @@
+//code - remove duplicate and only print the elemtns in a sorted array
+#include <iostream>
+#include <maps>
+using namespace std;
+int main (){
+    int size;
+    cin>>size;
+    int arr[size];
+    for(auto &it:arr){
+        cin>>it;
+    }
+    map <int,int> mp;
+    for(int i=0;i<size;i++){
+        mp[arr[i]]++;
+    }
+    for(auto &it:mp){
+        cout<<it.first<<" ";
+    }
+    return 0;
+}
