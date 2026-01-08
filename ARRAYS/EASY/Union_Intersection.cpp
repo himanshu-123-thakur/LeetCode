635b345af32ccd321449283837c9704f42551d09
Himanshu Thakur <himanthakur123@gmail.com>
2026-01-08 18:09:59 +0530

    UNION (Unique)& INTER 2 Pointers and comapare

--- ARRAYS/EASY/Union_Intersection.cpp
+++ ARRAYS/EASY/Union_Intersection.cpp
@@ -1,7 +1,9 @@
 //code
 //UNTION say two sorted arrays 
 // arr1[]={1,2,2,3,3,3,4,5,5,6} arr2[]={1,2,3,4,4,7}
-// UNION WILLE BE ARR[]={1,2,3,4,5,6,7}
+// UNION WILLE BE ARR[]={1,2,3,4,4,6,7}
+//INTERSECTION WILL BE RES[] ={1,2,3,4}
+
 
 #include <iostream>
 #include <set>
@@ -30,46 +32,73 @@ using namespace std;
 // o(n1+n2) 
 //BEAUTIFUL
 
-void union_arr(int arr1[], int arr2[], int size1, int size2){
-    vector<int>v;
-    int i=0;
-    int j= 0;
+// void union_arr(int arr1[], int arr2[], int size1, int size2){
+//     vector<int>v;
+//     int i=0;
+//     int j= 0;
 
-    while(i<size1 && j<size2){
-        if (arr1[i]<=arr2[j]){
-        if (v.size()==0 || v.back()!=arr1[i]){
-            v.push_back(arr1[i]);
-            }
-            i++;
-        }
+//     while(i<size1 && j<size2){
+//         if (arr1[i]<=arr2[j]){
+//         if (v.size()==0 || v.back()!=arr1[i]){
+//             v.push_back(arr1[i]);
+//             }
+//             i++;
+//         }
     
-        else{
-            if(v.size()==0 || v.back()!=arr2[j]){
-                    v.push_back(arr2[j]);
-                }
-                j++;
-            }
-        }
+//         else{
+//             if(v.size()==0 || v.back()!=arr2[j]){
+//                     v.push_back(arr2[j]);
+//                 }
+//                 j++;
+//             }
+//         }
 
-        while(j<size2){
-            if(v.size()==0 || v.back()!=arr2[j]){
-                v.push_back(arr2[j]);
-            }
-            j++;
-        }
+//         while(j<size2){
+//             if(v.size()==0 || v.back()!=arr2[j]){
+//                 v.push_back(arr2[j]);
+//             }
+//             j++;
+//         }
 
-        while(i<size1){
-            if(v.size()==0 || v.back()!=arr1[i]){
+//         while(i<size1){
+//             if(v.size()==0 || v.back()!=arr1[i]){
+//                 v.push_back(arr1[i]);
+//             }
+//             i++;
+//         }
+
+
+//         for(auto &it:v){
+//             cout<<it<<" ";
+//         }
+
+// }
+
+//BRUTE
+
+void inter_arr(int arr1[],int arr2[], int n1,int n2){
+    vector <int>v;
+    int arr[n2]={0};
+
+    int i=0;
+    while(i<n1){
+        for(int j =0;j<n2;j++){
+            if(arr1[i]==arr2[j] && arr[j]==0){
                 v.push_back(arr1[i]);
+                arr[j]=1;
+                i++;
+                break;
             }
-            i++;
+            if(arr1[i]<arr2[j]){
+                i++;
+                break;
+            }
         }
+    }
 
-
-        for(auto &it:v){
-            cout<<it<<" ";
-        }
-
+    for(auto &it:v){
+        cout<<it<<" ";
+    }
 }
 
 
@@ -78,6 +107,7 @@ int main(){
     int arr1[10]={1,2,2,3,3,3,4,5,5,6};
     int arr2[6]={1,2,3,4,4,7};
 
-    union_arr(arr1,arr2,10,6);
+    //union_arr(arr1,arr2,10,6);
+    inter_arr(arr1,arr2,10,6);
     return 0;
 }
