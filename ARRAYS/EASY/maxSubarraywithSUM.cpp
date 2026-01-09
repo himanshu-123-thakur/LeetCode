d98cc6cb2b459355bec831057d09aadfd753739f
Himanshu Thakur <himanthakur123@gmail.com>
2026-01-09 16:22:22 +0530

    HASHING VALA PART IMPORTANT REVISCE AGAIN

--- ARRAYS/EASY/maxSubarraywithSUM.cpp
+++ ARRAYS/EASY/maxSubarraywithSUM.cpp
@@ -21,41 +21,82 @@ using namespace std;
 // }
 
 
-//BETTER APPROACH  //DOES NOT WORK for 0's and -ve
-void longestsubarraywithK(int arr[], int size, int k){
-    map<long long,long long> mp;
-    int sum=0;
-    int length =0;
-    for(int i =0;i<size;i++){
-        sum +=arr[i];
-        // mp[sum]=i;
+//BETTER APPROACH  //DOES NOT WORK for 0's and -ve // for that we have added a little one down 
+// THis is the optimal solution for -ve and arrays 
+// for +ves we can do better
+// void longestsubarraywithK(int arr[], int size, int k){
+//     map<long long,long long> mp;
+//     int sum=0;
+//     int length =0;
+//     for(int i =0;i<size;i++){
+//         sum +=arr[i];
+//         // mp[sum]=i;
 
-        // Case 1: subarray from 0..i
-        if(sum == k){
-            length = max(length, i + 1);
-        }
+//         // Case 1: subarray from 0..i
+//         if(sum == k){
+//             length = max(length, i + 1);
+//         }
         
 
-        int rem = sum -k;
-        if(mp.find(rem)!=mp.end()){
-            int l = i-mp[rem];
-            length = max(length,l);
-        }
-        //TO cover case of 0 //VERY IMPORTANT
-        if(mp.find(sum)==mp.end())
-        {
-            mp[sum]=i;
-        } 
-    }
-    // for(auto &it:mp){
-    //     cout<<it.first<<" "<<it.second<<endl;
+//         int rem = sum -k;
+//         if(mp.find(rem)!=mp.end()){
+//             int l = i-mp[rem];
+//             length = max(length,l);
+//         }
+//         //TO cover case of 0 //VERY IMPORTANT
+//         if(mp.find(sum)==mp.end())
+//         {
+//             mp[sum]=i;
+//         } 
+//     }
+//     // for(auto &it:mp){
+//     //     cout<<it.first<<" "<<it.second<<endl;
+//     // }
+
+//     cout<<length<<endl;
+//     //NOW 
+
+// }
+
+
+//OPTIMAL - 2 POINTER only for +ve
+void longestsubarraywithK(int arr[], int n, int k){
+    // int i = 0;
+    // long long sum = 0;
+    // int best = 0;
+
+    // for(int j = 0; j < n; j++){
+    //     sum += arr[j];                 // expand window by including arr[j]
+
+    //     while(i <= j && sum > k){      // shrink from left until sum <= k
+    //         sum -= arr[i];
+    //         i++;
+    //     }
+
+    //     if(sum == k){
+    //         best = max(best, j - i + 1);
+    //     }
     // }
+    // cout<<best;
 
-    cout<<length<<endl;
-    //NOW 
 
+    int j = 0;
+    long long sum = 0;
+    int best = 0;
+
+    for(int i =0;i<n;i++){  //i goes forwars in sum 
+        sum+=arr[i];
+        for(;j<=i && sum>k ;j++){ //start to shrink from left if sum>k ,
+            if(sum>k){
+                sum -=arr[j];
+            }
+        }
+        if(sum==k){
+            best = max(best,i-j+1);
+        }
+    }
+    cout<<best;
 }
-
 
 
 int main(){
