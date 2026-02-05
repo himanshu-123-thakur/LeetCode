6970cc1e7668f2b345a74b2627a0d77b4f767a73
Himanshu Thakur <himanthakur123@gmail.com>
2026-02-05 11:24:47 +0530

    LATER

---
+++ Binary_Search/BS_Answers/sqrtUsingBS.cpp
@@ -0,0 +1,16 @@
+//SQRT using BS (logN)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int sqrt(int n){
+    int ans = 1;
+    
+}
+int main (){
+    int n;
+    cout<<"Enter the no : "
+    cin>>n;
+    cout<<"Sqrt of "<<n<<" is "<<sqrtof(n)<<endl;
+    return 0;
+}
