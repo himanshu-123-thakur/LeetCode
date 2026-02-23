6c9658226a4a9080a65c1c84d13c0c309fa62246
Himanshu Thakur <himanthakur123@gmail.com>
2026-02-23 21:06:28 +0530

    sqrt

--- Binary_Search/BS_Answers/sqrtUsingBS.cpp
+++ Binary_Search/BS_Answers/sqrtUsingBS.cpp
@@ -3,14 +3,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sqrt(int n){
-    int ans = 1;
+void sqrt(int n){
+    //WILLL WRITE LATER 
+    int low = 1;
+    int high = n;
+    int ans=0;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(mid*mid > n){
+            high = mid-1;
+        }
+        else if(mid*mid<n){
+            ans = low;
+            low = mid+1;
+        }
+        else{
+            ans =  mid;
+            break;
+        }
+    }
+    cout<<"sqrt is = "<<ans<<endl;
     
+
 }
 int main (){
     int n;
-    cout<<"Enter the no : "
+    cout<<"Enter the no : ";
     cin>>n;
-    cout<<"Sqrt of "<<n<<" is "<<sqrtof(n)<<endl;
+    // cout<<"Sqrt of "<<n<<" is "<<sqrt(n)<<endl;
+    sqrt(n);
     return 0;
 }
